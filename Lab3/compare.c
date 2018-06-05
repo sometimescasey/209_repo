470fdf932c1cd1784d866173fbe2dd8cff247201
Casey Li <sometimescasey@gmail.com>
2018-06-04 21:25:15 -0400

    done compare.c

--- Lab3/compare.c
+++ Lab3/compare.c
@@ -12,3 +12,32 @@
 
   Your main function should return 0, regardless of what is printed.
 */
+
+int main(int argc, char **argv) {
+	char *str1;
+	char *str2;
+
+	if (argv[1] != NULL && argv[2] != NULL && argv[3] == NULL) {
+		str1 = malloc(strlen(argv[1]) + 1); // plus one for ending null
+		str2 = malloc(strlen(argv[2]) + 1); // plus one for ending null
+		strcpy(str1, argv[1]);
+		strcpy(str2, argv[2]);
+
+		int result = strcmp(str1, str2);
+		
+		if (result == 0) {
+			printf("Same\n");
+		}
+		else {
+			printf("Different\n");
+		}
+		free(str1);
+		free(str2);
+	}
+	else {
+		printf("Invalid\n");
+	}
+	
+
+	return 0;
+}
