d3a601bbdc8414481c5142db1d2ff4ef4fd497a2
Casey Li <sometimescasey@gmail.com>
2018-07-09 18:19:47 -0400

    add childcreates.c

---
+++ Lab6/childcreates.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    int i;
+    int iterations;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: forkloop <iterations>\n");
+        exit(1);
+    }
+
+    iterations = strtol(argv[1], NULL, 10);
+
+    int spawned = 0;
+    for (i = 0; i < iterations; i++) {
+        if (spawned == 0) {
+            int n = fork();
+            if (n < 0) {
+                perror("fork");
+                exit(1);
+            }
+            if (n > 0) {
+                spawned = 1;
+            }
+        }
+        printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
+    }
+
+    return 0;
+}
